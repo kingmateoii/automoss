1bfe18a1300de231e4492d93c690d2b170d398fc
kingmateoii <75754407+kingmateoii@users.noreply.github.com>
2024-09-06 00:15:08 -0500

    updated email settings

--- automoss/settings.py
+++ automoss/settings.py
@@ -18,6 +18,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == 'True'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")  # Replace with App Password
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+
+DEFAULT_FROM_EMAIL = "matthewragsdale1935@gmail.com"
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
